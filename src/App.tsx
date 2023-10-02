8419d84bd52c4d4f78eadba15179578e716f8a6f
Mateus Israel <mateus@pop-os.localdomain>
2023-10-02 00:55:58 -0300

    Websocket - troca de mensagens entre instancias

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { client } from "./services/supabase.ts";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<string[]>([]);
+
+  const handleReceiveMessage = (payload: any) => {
+    const t = [...messages, payload.payload.message];
+    console.log(t);
+
+    setMessages(t);
+  };
+
+  const channel = client.channel("chat");
+  channel
+    .on("broadcast", { event: "message" }, handleReceiveMessage)
+    .subscribe();
+
+  useEffect(() => {}, []);
+
+  const handleSendMessage = () => {
+    if (!message) return;
+
+    channel.send({
+      type: "broadcast",
+      event: "message",
+      payload: { message: message },
+    });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="messages">
+        {messages.map((i: string) => {
+          return <div>{i}</div>;
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <input
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        type="text"
+      />
+      <button onClick={handleSendMessage}>Enviar</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+// const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
